7ba844260083063aabd72d3e5d694ad31135a426
Nguyen Quang Huy <quanghuy190105@gmail.com>
2025-09-29 10:15:31 +0700

    add spring security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String Hello() {
-        return "Hello World";
+    public String index() {
+        return "Hello World !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello user !";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello admin !";
     }
 }
