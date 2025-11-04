7cf20ba1ec214f5d7f9f0013109421a0c7a7da81
Nguyen Quang Huy <quanghuy190105@gmail.com>
2025-11-04 09:57:50 +0700

    Add vid 90 # 79

--- src/main/java/vn/hoidanit/laptopshop/domain/Role.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -1,20 +1,29 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
-    private long id;
-    private String name;
-    private String description;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String description;
+
+    // role one -> to many -> user
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+
     public long getId() {
         return id;
     }
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,9 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,14 @@ public class User {
     private String avatar;
 
     // roleID
+    // User many -> To One -> role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    // user -> oneToMany -> order
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     public void setId(long id) {
         this.id = id;
